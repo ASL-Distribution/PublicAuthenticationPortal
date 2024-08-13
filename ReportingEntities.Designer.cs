99656f619d1e2070e28fa9b8cea008da88507ed3
ajafridev <ajafri@neuronicworks.com>
2024-08-13 13:31:17 -0400

    .net framework version upgraded.

--- ReportingEntities.Designer.cs
+++ ReportingEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ahsan\Dropbox\Consulting\Clients\ASL\Projects\Authentication\VS Projects\PublicAuthenticationPortal\ReportingEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ahsan\Source\Repos\ASL-Distribution\PublicAuthenticationPortal\ReportingEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
